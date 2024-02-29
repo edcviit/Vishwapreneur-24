c73ca705204c38bae0e76858fe57f67485a14a86
vaishnavi Sonawane <vaishnavisonawane1106@gmail.com>
2024-02-29 19:17:33 +0530

    partners seperate page

--- src/components/homepage/SponsorsMedia/MediaPartner1.jsx
+++ src/components/homepage/SponsorsMedia/MediaPartner1.jsx
@@ -6,60 +6,45 @@ const SponsorMedia1 = () => {
     <div>
       <div className="headContainer">
         <h1>SPONSORS</h1>
+        <h1>MEDIA PARTNERS</h1>
       </div>
-      <div className="sponsors">
-        <div className="main">
-          <a href="https://tradewithjazz.com/">
-            <div className="logoimg">
-              <img
-                className="tokenImage"
-                src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003322/Sponsors_webp/ezflynldz6c5e1pnbcw3.webp"
-                alt="title Sponsor"
-              />
-            </div>
-          </a>
-          <h2>BMW</h2>
-          <hr />
-          <p className="description">TITLE SPONSOR</p>
-          <hr />
-        </div>
-      </div>
+
       <div className="level2">
         <div className="sponsors">
           <div className="main">
-            <a href="https://thekiranacademy.com/">
+            <a href="https://www.esakal.com/">
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003324/Sponsors_webp/oujirgalpbgpkr66aipo.webp"
-                  alt="time"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003436/Parteners/y2gedmgbptiikgp4auev.webp"
+                  alt="ABP"
                 />
               </div>
             </a>
-            <h2>T.I.M.E</h2>
+            <h2>ABP MAJHA</h2>
             <hr />
-            <p className="description">POWERED SPONSOR</p>
+            <p className="description">BROADCASTING PARTNER</p>
             <hr />
           </div>
         </div>
         <div className="sponsors">
           <div className="main">
-            <a href="https://herody.in/">
+            <a href="https://www.esakal.com/">
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003322/Sponsors_webp/daklik83d25fpbjhcptm.webp"
-                  alt="abhibus"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003437/Parteners/hpyyofmhvq05bzfkthmi.webp"
+                  alt="sakal"
                 />
               </div>
             </a>
-            <h2>ABHIBUS</h2>
+            <h2>SAKAL</h2>
             <hr />
-            <p className="description">POWERED SPONSOR</p>
+            <p className="description">Media Partner</p>
             <hr />
           </div>
         </div>
-        <div className="sponsors">
+        {/* <div className="sponsors">
           <div className="main">
             <a href="https://www.hdfccredila.com/">
               <div className="logoimg">
@@ -75,40 +60,40 @@ const SponsorMedia1 = () => {
             <p className="description">FUNDING PARTNER</p>
             <hr />
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="level3">
         <div className="sponsors">
           <div className="main">
-            <a href="https://www.aeccglobal.in/">
+            <a href="https://www.saamana.com/">
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003322/Sponsors_webp/d9dkdoowhgdqovyoada2.webp"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003435/Parteners/p3tnwcixgvziwxkgtqjy.webp"
                   alt="balaji"
                 />
               </div>
             </a>
-            <h2>BALAJI WAFERS</h2>
+            <h2>SAMANA</h2>
             <hr />
-            <p className="description">CO-POWERED SPONSOR</p>
+            <p className="description">PRINTING PARTNER</p>
             <hr />
           </div>
         </div>
         <div className="sponsors">
           <div className="main">
-            <a href="https://www.newstrides.com/">
+            <a href="https://www.dainikprabhat.com/">
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003322/Sponsors_webp/pvxaq7j6hwzpvcwsvrwg.webp"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003436/Parteners/fu4otcyc1ijpjcdkvyxy.webp"
                   alt="budhani"
                 />
               </div>
             </a>
-            <h2>BUDHANI BROS</h2>
+            <h2>PRABHAT</h2>
             <hr />
-            <p className="description">CO-POWERED SPONSOR</p>
+            <p className="description">PUBLISHING PARTNER</p>
             <hr />
           </div>
         </div>
@@ -127,18 +112,18 @@ const SponsorMedia1 = () => {
         </div> */}
         <div className="sponsors">
           <div className="main">
-            <a href="https://www.imfs.co.in/">
+            <a href="http://kumarpacificmall.com/">
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708014297/Sponsors_webp/j209kuroisn3hd0scp9y.png"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003435/Parteners/uft1xsnuzizuxnagbjgk.webp"
                   alt="ed wise"
                 />
               </div>
             </a>
-            <h2>IMFS</h2>
+            <h2>KUMAR PACIFIC</h2>
             <hr />
-            <p className="description">CO-POWERED SPONSER</p>
+            <p className="description">MALL PARTNER</p>
             <hr />
           </div>
         </div>
@@ -146,18 +131,18 @@ const SponsorMedia1 = () => {
       <div className="level4">
         <div className="sponsors">
           <div className="main">
-            <a href="https://www.edwiseinternational.com/">
+            <a href="https://unstop.com/">
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003322/Sponsors_webp/oafgn40qgljd2ydfts2w.webp"
-                  alt="ed wise"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708018109/Parteners/fz8l9u3jmflviqzwyukt.png"
+                  alt="unstop"
                 />
               </div>
             </a>
-            <h2>Coding Ninjas</h2>
+            <h2>UNSTOP</h2>
             <hr />
-            <p className="description">GOLD SPONSOR</p>
+            <p className="description">OUTREACH PARTENER</p>
             <hr />
           </div>
         </div>
@@ -167,74 +152,145 @@ const SponsorMedia1 = () => {
               <div className="logoimg">
                 <img
                   className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708002954/PRs_Webp/zlcgbrfmfftrvcwjah3e.webp"
-                  alt="ed wise"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1709207605/Bhau_New_rmhjba.jpg"
+                  alt="BHAU"
                 />
               </div>
             </a>
-            <h2>AARNELL</h2>
+            <h2>BHAU INSTITUTE</h2>
             <hr />
-            <p className="description">GOLD SPONSOR</p>
+            <p className="description">INCUBATION PARTENER</p>
             <hr />
           </div>
         </div>
 
-        {/* <div className="sponsors">
+        <div className="sponsors">
           <div className="main">
-            <a href="https://scarters.com/">
+            <a href="https://www.campustimespune.com/">
               <div className="logoimg">
-                <img className="tokenImage" src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003324/Sponsors_webp/f9wchu6pvannqhnxom7z.webp" alt="ed wise" />
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003435/Parteners/wvwcgh8xmxawuypxxugl.webp"
+                  alt="camppus"
+                />
               </div>
             </a>
-            <h2>SCATTERS</h2>
+            <h2>CAMPUS TIMES PUNE</h2>
             <hr />
-            <p className="description">LUXARY PARTNER</p>
+            <p className="description">YOUTH PARTENER</p>
             <hr />
           </div>
-          
+        </div>
+      </div>
+      <div className="level4">
+        <div className="sponsors">
+          <div className="main">
+            <a href="https://businesssphere.co.in/">
+              <div className="logoimg">
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003436/Parteners/sxeebenslrzkugnafkxv.webp"
+                  alt="unstop"
+                />
+              </div>
+            </a>
+            <h2>BUSINESS SPHERE</h2>
+            <hr />
+            <p className="description">MAGAZINE PARTENER</p>
+            <hr />
+          </div>
+        </div>
+        <div className="sponsors">
+          <div className="main">
+            <a href="https://theglobalhues.com/">
+              <div className="logoimg">
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708017541/Parteners/eoptm6fv7sersqfmxakb.png"
+                  alt="BHAU"
+                />
+              </div>
+            </a>
+            <h2>THE GLOBAL HUES</h2>
+            <hr />
+            <p className="description">MAGAZINE PARTENER</p>
+            <hr />
+          </div>
+        </div>
+
+        <div className="sponsors">
+          <div className="main">
+            <a href="https://www.campustimespune.com/">
+              <div className="logoimg">
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1709209449/JPEG-startup-success-stories_tgszy7.jpg"
+                  alt="camppus"
+                />
+              </div>
+            </a>
+            <h2>STARTUP SUCCESS STORY</h2>
+            <hr />
+            <p className="description">ONLINE MEDIA PARTENER</p>
+            <hr />
+          </div>
+        </div>
+      </div>
+      <div className="level4">
+        <div className="sponsors">
+          <div className="main">
+            <a href="https://businesssphere.co.in/">
+              <div className="logoimg">
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1709212111/Logo_250_200_viwxgn.png"
+                  alt="KAF"
+                />
+              </div>
+            </a>
+            <h2>KNOW A FEST</h2>
+            <hr />
+            <p className="description">ONLINE MEDIA PARTENER</p>
+            <hr />
+          </div>
+        </div>
+        <div className="sponsors">
+          <div className="main">
+            <a href="https://www.thehighereducationreview.com/">
+              <div className="logoimg">
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1709212647/Screenshot_2024-02-29_184639_gjbaxc.png"
+                  alt="EOI"
+                />
+              </div>
+            </a>
+            <h2>HIGHER EDUCATION REVIEW</h2>
+            <hr />
+            <p className="description">EDUCATION PARTENER</p>
+            <hr />
+          </div>
+        </div>
+{/* 
+        <div className="sponsors">
+          <div className="main">
+            <a href="https://startuptalky.com/">
+              <div className="logoimg">
+                <img
+                  className="tokenImage"
+                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003435/Parteners/j65lz2mbx2piln7kz2jr.webp"
+                  alt="camppus"
+                />
+              </div>
+            </a>
+            <h2>STARTUP TALKY</h2>
+            <hr />
+            <p className="description">ONLINE MEDIA PARTENER</p>
+            <hr />
+          </div>
         </div> */}
-
-        <div className="sponsors">
-          <div className="main">
-            <a href="https://www.edwiseinternational.com/">
-              <div className="logoimg">
-                <img
-                  className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003322/Sponsors_webp/ooz63xbjc9jfjp9j0yww.webp"
-                  alt="ed wise"
-                />
-              </div>
-            </a>
-            <h2>ED WISE</h2>
-            <hr />
-            <p className="description">GOLD SPONSOR</p>
-            <hr />
-          </div>
-        </div>
       </div>
-      <div className="level14">
-        
-        <div className="sponsors">
-          <div className="main">
-            <a href="https://scarters.com/">
-              <div className="logoimg">
-                <img
-                  className="tokenImage"
-                  src="https://res.cloudinary.com/di40y80dw/image/upload/v1708003324/Sponsors_webp/f9wchu6pvannqhnxom7z.webp"
-                  alt="ed wise"
-                />
-              </div>
-            </a>
-            <h2>SCATTERS</h2>
-            <hr />
-            <p className="description">LUXARY PARTNER</p>
-            <hr />
-          </div>
-        </div>
-
-        
-        
-      </div>
+      
       
     </div>
   );
