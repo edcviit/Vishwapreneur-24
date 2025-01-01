43e9f3b245eba564b592297b25e2e2c9ca5dbd99
AayushShirode2060 <ayushshirode2060@gmail.com>
2025-01-01 17:55:34 +0530

    changes done in Footer and Review

--- src/components/homepage/Footer/Footer.jsx
+++ src/components/homepage/Footer/Footer.jsx
@@ -3,25 +3,53 @@ import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
 import './Footer.css'
 import image2 from '../../../assets/footer 1.png'
 import "@fortawesome/fontawesome-free/css/all.css";
+import { Center } from '@react-three/drei';
 
 const Footer = () => {
   return (
+    
+
     <div
       className={` relative footer-section  mt-24`}
       // className={` relative footer-section border-[1px] border-slate-300   `}
     >
+     
       {/* <div className='bg-gradient-to-b from-black via-transperent to-white w-fit h-fit w-'> */}
       {/* <img src={image2} className=' absolute  bottom-0 h-[39rem] md:h-[50rem]  object-cover md:object-fill md:w-[100vw]  opacity-85' style={{zIndex:"-1",webkitMaskImage: "WebkitGradient(linear, top,  bottom,from(rgba(0,0,0,0)) to(rgba(0,0,0,1)))",backgroundImage: "linear-gradient(to bottom,#000000,#000000, #6766b4,#bb57a1)" }} alt="" /> */}
       {/* <img src={image2} className=' absolute -z-10 bottom-0 h-[39rem] md:h-[50rem] w-[100vw] ' style={{ webkitMaskImage: "-webkit-gradient(linear, top,  bottom,from(rgba(0,0,0,0)),via(rgba(0,0,0,0.5)), to(rgba(0,0,0,1)))"}} alt="" /> */}
       {/* </div> */}
+      
       <footer className=" text-white py-12 border rounded-t-3xl backdrop-blur-sm border-white p-2">
         {/* <div className={`z-50    `} style={{ backgroundImage: `url(${image})`, width:screen, height:"20rem"}}></div> */}
         <div className={`w-full h-full  relative m-15 `}>
           <div className="container mx-auto flex flex-col items-center">
             {/* Grid layout and content */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-8">
+              <div className="mx-auto flex flex-col items-center flex-wrap">
+      
+                 <img src="https://www.knowafest.com/files/uploads/Asset%2013-2023012705.png" alt="VIshwapreneur-24" className="VP-img" />
+                  {/* <p>The Entrepreneurship Development Cell (EDC) is a student-run organisation that promotes and nurtures an entrepreneurial spirit among students. It serves as a platform for aspiring entrepreneurs to explore and develop their business ideas, gain practical skills, and connect with industry experts and mentors.</p>  */}
+              </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 mb-0\.5">
+            
+            <div className="text-center ml-2 md:text-left lg:text-left xl:text-left">
+                <p
+                  className="font-bold mb-2 text-lg hover:underline"
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(to right, #bb57a1, #6766b4)",
+                    WebkitBackgroundClip: "text",
+                    color: "transparent",
+                  }}
+                >
+                  Info
+                </p>
+                
+                <p>The Entrepreneurship Development Cell (EDC) is a student-run organisation that promotes and nurtures an entrepreneurial spirit among students.</p>
+              </div>
+
               {/* Quick Links Section */}
-              <div className="text-center ml-2 md:text-left lg:text-left xl:text-left">
+              
+              <div className="text-center col-span-2 sm:col-span-1 transition  md:ml-1 lg:md-2 duration-300">
                 <p
                   className="font-bold mb-2 text-lg hover:underline"
                   style={{
@@ -35,25 +63,25 @@ const Footer = () => {
                 </p>
                 {/* ... (rest of the Quick Links section) */}
                 <ul className="list-none">
-                  <li className="mb-2 hover:scale-105">
+                  <li className="mb-2 hover:scale-105 duration-300 transition">
                     <a href="https://learner.vierp.in/events" target="_blank">
                       Register
                     </a>
                   </li>
-                  <li className="mb-2 hover:scale-105">
+                  <li className="mb-2 hover:scale-105 duration-300 transition">
                     <a href="https://edcviit.com/ourteam">Our Team</a>
                   </li>
                   {/* <li className="mb-2 hover:scale-105">
                       <a href="/#termsConditions">Privacy Policy</a>
                     </li> */}
-                  <li className="mb-2 hover:scale-105">
+                  <li className="mb-2 hover:scale-105 duration-300 transition">
                     <a href="/#termsConditions">Terms and Conditions</a>
                   </li>
                 </ul>
               </div>
 
               {/* Follow Us Section */}
-              <div className="col-span-2 sm:col-span-1 transition ml-16 md:ml-1 lg:md-2 duration-300">
+              <div className="text-center col-span-2 sm:col-span-1 transition  md:ml-1 lg:md-2 duration-300">
                 <p
                   className="font-bold mb-2 text-lg transform hover:underline"
                   style={{
@@ -66,18 +94,18 @@ const Footer = () => {
                   Follow us on
                 </p>
                 {/* ... (rest of the Follow Us section) */}
-                <div className="flex space-x-4 -ml-4">
+                <div className="textflex space-x-4 -ml-4">
                   <a href="https://www.instagram.com/edcviit/" target="_blank">
-                    <i className="fa-brands fa-instagram text-2xl transition duration-300 transform hover:scale-125"></i>
+                    <i className="fa-brands fa-instagram text-2xl transition duration-300 transform hover:scale-125 instagram"></i>
                   </a>
                   <a
                     href="https://www.linkedin.com/company/entrepreneurship-development-cell-viit/"
                     target="_blank"
                   >
-                    <i class="fa-brands fa-linkedin-in text-2xl transition duration-300 transform hover:scale-125"></i>
+                    <i class="fa-brands fa-linkedin-in text-2xl transition duration-300 transform hover:scale-125 linkedin"></i>
                   </a>
                   <a href="https://twitter.com/edcviit" target="_blank">
-                    <i class="fa-brands fa-x-twitter text-2xl transition duration-300 transform hover:scale-125"></i>
+                    <i class="fa-brands fa-x-twitter text-2xl transition duration-300 transform hover:scale-125 twitter"></i>
                   </a>
                   <a href="https://www.youtube.com/@edcviit" target="_blank">
                     <i
@@ -86,7 +114,8 @@ const Footer = () => {
                       transition
                       duration-300
                       transform
-                      hover:scale-125"
+                      hover:scale-125
+                      youtube"
                     ></i>
                   </a>
                 </div>
@@ -106,13 +135,13 @@ const Footer = () => {
                   Contact us
                 </p>
                 {/* ... (rest of the Contact Us section) */}
-                <p className="hover:scale-105">
-                  Email:<a href="mailto:edc@viit.ac.in"> edc@viit.ac.in</a>
+                <p className="hover:scale-105 duration-300 transition">
+                  Email:<a href="Fmailto:edc@viit.ac.in"> edc@viit.ac.in</a>
                 </p>
-                <p className="hover:scale-105">
+                <p className="hover:scale-105 duration-300 transition">
                   Phone:<a href="tel:+919561211033"> +91 9561211033</a>
                 </p>
-                <p className="hover:scale-105">
+                <p className="hover:scale-105 duration-300 transition">
                   Address:
                   <a href="https://maps.app.goo.gl/XJaQ351o7yRLJFrHA">
                     {" "}
@@ -121,15 +150,19 @@ const Footer = () => {
                 </p>
               </div>
 
-              {/* Download App Section */}
-              <div className="text-center transition duration-300 transform">
+             
+           
+              
+                   {/* Download App Section */}
+              <div className="text-center  downloadApp transition duration-300 transform">
                 <p
-                  className="font-bold mb-2 text-lg"
+                  className=" font-bold mb-2 text-lg "
                   style={{
                     backgroundImage:
                       "linear-gradient(to right, #bb57a1, #6766b4)",
                     WebkitBackgroundClip: "text",
                     color: "transparent",
+                     
                   }}
                 >
                   Download Our App
@@ -140,13 +173,14 @@ const Footer = () => {
                   target="_blank"
                   className="text-lg border-b border-white hover:border-opacity-50 "
                 >
-                  <span className="flex items-center hover:scale-105">
+                  <span className="text-center flex items-center justify-center hover:scale-105 duration-300 transition">
                     <i className="fab fa-google-play pr-2"></i>
                     <h6>Download from Playstore</h6>
                   </span>
                 </a>
               </div>
-            </div>
+                  
+              </div>
           </div>
         </div>
       </footer>
--- src/components/homepage/Reviews/Reviews.jsx
+++ src/components/homepage/Reviews/Reviews.jsx
@@ -1,135 +1,57 @@
-import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./Reviews.css"; // Import your CSS file for Reviews
+import React, { useState, useEffect } from 'react';
+import './Reviews.css';
 
-const reviewsData = [
-  {
-    id: 1,
-    name: " Nikhil Sharma",
-    opinion:
-      "It was nice, it was fun. Lot of questions were asked and I loved to answer those questions.",
-    image:
-      "https://res.cloudinary.com/da5mmxnn3/image/upload/v1647506687/Speakers/nikhil_qyuop3.jpg",
-  },
-  {
-    id: 2,
-    name: "Prajakta Koli",
-    opinion:
-      "First of all, I love college events, I come for the energy, Ifeel like everyone's so mad. So thank you so much for having me and the team is very hardworking, very sweet.Thanks you for making the last minute arrangements.",
-    image:
-      "https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510541/Imagescopy/koli_k4yfwv.jpg",
-  },
-  {
-    id: 3,
-    name: "Gaurav Chaudhary",
-    opinion:
-      "Yahan jo charm dikh raha hai jo fire hai matlab iss event ki jo complete concept hai wo unique hai. In TG style : Bahot hi badhiya event tha aur isme jo seati thi vo bhi kaafi achi thi kafi badhiya tha, bole toh full value for time.",
-    image:
-      "https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510544/Imagescopy/Gaurav_uxkq4d.jpg",
-  },
-  {
-    id: 4,
-    name: "Rashmi Bansal",
-    opinion:
-      "I really congratulate the team for bringing together so many diverse students and so many good speakers I do hope that this goes further and many many students start a small business of their own to really get a taste of entrepreneurship for themselves!",
-    image:
-      "https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510552/Imagescopy/rashmi2_q0shh6.webp",
-  },
-  {
-    id: 5,
-    name: "Zubin Sharma",
-    opinion:
-      "I think the team has done an amazing job of using the spirit of entrepreneurship which is there in EDC to create a really really wonderful event for everyone involved from students to the panelists.",
-    image:
-      "https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510587/Imagescopy/zubin1_ydwtyq.webp",
-  },
-
-  // Add more review objects similarly
+const REVIEWS = [
+  // { id: 6, name: 'Alice', role: 'Data Scientist', avatar: 'https://i.pravatar.cc/150?img=23', review: `Absolutely mind-blowing! From graphics to gameplay, it's a virtual masterpiece. I lost track of time in the immersive experience.` },
+  { id: 0, name: 'Nikhil Sharma', role: 'Mumbiker Nikhil', avatar: 'https://res.cloudinary.com/da5mmxnn3/image/upload/v1647506687/Speakers/nikhil_qyuop3.jpg', review: `It was nice, it was fun. Lot of questions were asked and I loved to answer those questions.` },
+  { id: 2, name: 'Prajakta Koli', role: 'Mostly Sane', avatar: 'https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510541/Imagescopy/koli_k4yfwv.jpg', review: `First of all, I love college events, I come for the energy, Ifeel like everyone's so mad. So thank you so much for having me and the team is very hardworking, very sweet.Thanks you for making the last minute arrangements.` },
+  { id: 3, name: 'Gaurav Chaudhary', role: 'Technical Guruji', avatar: 'https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510544/Imagescopy/Gaurav_uxkq4d.jpg', review: `Yahan jo charm dikh raha hai jo fire hai matlab iss event ki jo complete concept hai wo unique hai. In TG style : Bahot hi badhiya event tha aur isme jo seati thi vo bhi kaafi achi thi kafi badhiya tha, bole toh full value for time.` },
+  // { id: 13, name: 'Ethan', role: 'Software Engineer', avatar: 'https://i.pravatar.cc/150?img=57', review: `I really congratulate the team for bringing together so many diverse students and so many good speakers I do hope that this goes further and many many students start a small business of their own to really get a taste of entrepreneurship for themselves!` },
+  { id: 4, name: 'Rashmi Bansal', role: 'Non-fiction writer And Entrenpreneur', avatar: 'https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510552/Imagescopy/rashmi2_q0shh6.webp', review: `Plan, create, and explore seamlessly. This service made my creative dreams a reality. Smooth navigation, and the recommendations were spot on.` },
+  // { id: 10, name: 'George', role: 'Software Developer', avatar: 'https://i.pravatar.cc/150?img=21', review: `A game-changer for organization. Tasks, calendars, notes – everything neatly synced. My life has never been this streamlined. Pure genius!` },
+  // { id: 11, name: 'Hannah', role: 'Graphic Designer', avatar: 'https://i.pravatar.cc/150?img=18', review: `Drowning in a sea of creativity. The content here is a visual feast. An endless source of inspiration for my artistic endeavors.` },
+  { id: 5, name: 'Zubin Sharma', role: 'Founder and CEO at Project Potential', avatar: 'https://res.cloudinary.com/da5mmxnn3/image/upload/v1647510587/Imagescopy/zubin1_ydwtyq.webp', review: `I think the team has done an amazing job of using the spirit of entrepreneurship which is there in EDC to create a really really wonderful event for everyone involved from students to the panelists.` },
 ];
+
 const Reviews = () => {
-  const [currentReview, setCurrentReview] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
-  const [opacity, setOpacity] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [transitioning, setTransitioning] = useState(false);
 
   useEffect(() => {
-    let timeout;
+    const interval = setInterval(() => {
+      setTransitioning(true);
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % REVIEWS.length);
+        setTransitioning(false);
+      }, 500); // Match duration with CSS transition time
+    }, 5000); // Change slide every 5 seconds
 
-    if (!isPaused) {
-      timeout = setTimeout(() => {
-        setOpacity(0); // Fade out
-        setTimeout(() => {
-          const nextReview = (currentReview + 1) % reviewsData.length;
-          setCurrentReview(nextReview);
-          setOpacity(1); // Fade in
-        }, 500); // Wait for fade-out transition to complete
-      }, 4000);
-    }
+    return () => clearInterval(interval);
+  }, []);
 
-    return () => clearTimeout(timeout);
-  }, [currentReview, isPaused, reviewsData]);
-
-  const handlePause = () => {
-    setIsPaused(!isPaused);
-  };
-
-  const handleNext = () => {
-    setOpacity(0); // Fade out
-    setTimeout(() => {
-      const nextReview = (currentReview + 1) % reviewsData.length;
-      setCurrentReview(nextReview);
-      setOpacity(1); // Fade in
-    }, 500); // Wait for fade-out transition to complete
-  };
-
-  const handlePrevious = () => {
-    setOpacity(0); // Fade out
-    setTimeout(() => {
-      const prevReview =
-        (currentReview - 1 + reviewsData.length) % reviewsData.length;
-      setCurrentReview(prevReview);
-      setOpacity(1); // Fade in
-    }, 500); // Wait for fade-out transition to complete
-  };
+  const currentReview = REVIEWS[currentIndex];
 
   return (
-    <>
-      <div class="nine">
-        <h1 className="main-heading pt-28 bg-gradient-to-r from-[#b74b9b] to-[#ffb3eb] text-transparent bg-clip-text text-3xl font-bold text-center ">
-          Reviews
-        </h1>
-      </div>
+    <div className="reviews-container">
+       <h1 className="main-heading bg-gradient-to-r from-[#b74b9b] to-white text-transparent bg-clip-text text-4xl font-bold text-center py-8">Reviews</h1>
+      <h1 className="reviews-title">A word from our Guests</h1>
+      
 
-      <div className=" md:min-h-[40vh]  min-h-[50vh] max-w-screen-lg mx-auto mt-8 p-4 bg-transparent rounded-md shadow-md  flex flex-col gap-3 md:flex md:flex-row-reverse">
-        <div
-          className={`basis-1/2 flex justify-center transition-opacity duration-500 ease-in-out ${
-            opacity === 1 ? "" : "opacity-0"
-          }`}
-        >
+      <div className={`review-card ${transitioning ? 'transitioning' : ''}`}>
+        <blockquote className="review-text ${transitioning ? 'slide-up' : ''}    rounded-md">"{currentReview.review}"</blockquote>
+        <div className="review-details">
           <img
-            src={reviewsData[currentReview].image}
-            alt={reviewsData[currentReview].name}
-            className=" h-40 object-cover rounded-md "
+            src={currentReview.avatar}
+            alt={`${currentReview.name}'s avatar`}
+            className={`review-avatar ${transitioning ? 'slide-up' : ''}`}
           />
-        </div>
-        <div
-          className={`transition-opacity duration-500 ease-in-out flex flex-col flex-start ${
-            opacity === 1 ? "" : "opacity-0"
-          }`}
-        >
-          <blockquote>
-            <p className="text-2xl italic font-medium text-white ">
-              {`"${reviewsData[currentReview].opinion}"`}
-            </p>
-          </blockquote>
-
-          <h2 className="mt-6 text-lg text-violet-600 font-semibold mb-2 text-start">
-            {`~ ${reviewsData[currentReview].name}`}
-          </h2>
+          <div className={`review-info ${transitioning ? 'slide-up' : ''}`}>
+            <p className="review-name">{currentReview.name}</p>
+            <p className="review-role">{currentReview.role}</p>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
