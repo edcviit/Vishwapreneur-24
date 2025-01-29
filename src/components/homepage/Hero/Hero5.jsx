bd5b8b398b05ad64c9a8177baf6c0d35e0afcfb8
ParthKulat03 <kulatparth08@gmail.com>
2025-01-29 20:39:38 +0530

    Updated All

--- src/components/homepage/Hero/Hero5.jsx
+++ src/components/homepage/Hero/Hero5.jsx
@@ -1,106 +1,27 @@
-
-// import React, { useEffect } from 'react';
-// import { useRive, useStateMachineInput } from 'rive-react';
-// import "./Hero5.css";
-
-// function Hero5() {
-//   const STATE_MACHINE_NAME = "State Machine 1";
-//   const INPUT_NAME = "Switch";
-
-//   const { rive, RiveComponent } = useRive({
-//     src: "/rive/vp.riv",
-//     stateMachines: STATE_MACHINE_NAME,
-//     autoplay: true,
-//   });
-
-//   const onLoadFunction = useStateMachineInput(
-//     rive,
-//     STATE_MACHINE_NAME,
-//     INPUT_NAME
-//   );
-
-//   useEffect(() => {
-//     if (onLoadFunction) {
-//       onLoadFunction.fire(); 
-//     }
-//   }, [onLoadFunction]);
-
-//   return (
-//     <div>
-//       <h1 className="H1">Rive React Animation</h1>
-//       <RiveComponent />
-//     </div>
-//   );
-// }
-
-// export default Hero5;
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useRive } from '@rive-app/react-canvas';
-
-// export default function Hero5() {
-//   const { rive, RiveComponent } = useRive({
-//     src: 'vp.riv',
-//     stateMachines: "bumpy",
-//     autoplay: false,
-//   });
-
-//   return (
-//     <RiveComponent
-//       onMouseEnter={() => rive && rive.play()}
-//       onMouseLeave={() => rive && rive.pause()}
-//     />
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 import { useState, useEffect } from "react";
 
 const ResponsiveVideo = () => {
   const [videoSource, setVideoSource] = useState("/rive/new5.mp4"); // Default video for desktop
-
+  // console.log(videoSource, setVideoSource);
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
-
+      console.log(window.innerWidth)
       if (screenWidth <= 768) {
         // Mobile size
+        console.log("mobile");
+        
         setVideoSource("/rive/mobile.mp4");
       } else if (screenWidth <= 1024) {
         // Tablet size
+        console.log("tab");
+
         setVideoSource("/rive/tablet.mp4");
       } else {
         // Desktop size
+        console.log("desk");
+
         setVideoSource("/rive/new5.mp4");
       }
     };
@@ -119,11 +40,13 @@ const ResponsiveVideo = () => {
     <div className="relative w-screen h-screen overflow-hidden">
       {/* Video element */}
       <video
+        key={videoSource}  // 👈 Forces a re-render when the source changes
         className="absolute top-0 left-0 w-full h-full object-cover"
         autoPlay
         muted
         playsInline
       >
+        
         <source src={videoSource} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
@@ -132,7 +55,3 @@ const ResponsiveVideo = () => {
 };
 
 export default ResponsiveVideo;
-
-
-
-
